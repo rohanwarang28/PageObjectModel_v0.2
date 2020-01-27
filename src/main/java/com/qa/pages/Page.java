d3d027a411709b8c48fd035ce8e63c17104c4d5c
rohanwarang28 <rohanwarang28@yahoo.com>
2020-01-27 19:54:36 +0530

    minor changes

--- src/main/java/com/qa/pages/Page.java
+++ src/main/java/com/qa/pages/Page.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+//@author:rohan 
 public abstract class Page {
 	
 	public static WebDriver driver;
